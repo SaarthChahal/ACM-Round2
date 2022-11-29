f01e34b69098947c6cc97783e5f37379abd590dd
Saarth Chahal <93906832+SaarthChahal@users.noreply.github.com>
2022-11-29 18:12:08 +0530

    Add files via upload

---
+++ WORDLE.py
@@ -0,0 +1,13 @@
+# cook your dish here
+T=int(input())
+for i in range(T):
+    S=(input())
+    T=(input())
+    for j in range (5):
+        m=''
+        if S[j]==T[j]:
+            m+='G'
+        else:
+            m+='B'
+        print(m,end='')
+    print('')
